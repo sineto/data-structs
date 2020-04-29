6c327a00f83ec2c3060820d595ea8cbb9faff266
Sinésio Neto <sn.logbackup@gmail.com>
2020-04-29 16:32:23 -0300

    Update main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/sineto/data-structs/fifo"
 	"github.com/sineto/data-structs/lifo"
 )
 
@@ -13,10 +14,19 @@ func main() {
 	stack.Push(&lifo.Item{Value: 3})
 
 	fmt.Println(stack.Items)
-	stack.Pop()
-	fmt.Println(stack.Items)
-	stack.Pop()
-	fmt.Println(stack.Items)
-	stack.Pop()
-	fmt.Println(stack.Items)
+	for i := 0; i < 3; i++ {
+		stack.Pop()
+		fmt.Println(stack.Items)
+	}
+
+	queue := fifo.NewQueue()
+	queue.Push(&fifo.Item{Value: 1})
+	queue.Push(&fifo.Item{Value: 2})
+	queue.Push(&fifo.Item{Value: 3})
+
+	fmt.Println(queue.Items)
+	for i := 0; i < 3; i++ {
+		queue.Shift()
+		fmt.Println(queue.Items)
+	}
 }
