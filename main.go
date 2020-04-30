cffb7d4a4005efa6889f17583da50ce30fa162e8
Sinésio Neto <sn.logbackup@gmail.com>
2020-04-29 22:17:37 -0300

    Update main.go

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 
 	"github.com/sineto/data-structs/fifo"
 	"github.com/sineto/data-structs/lifo"
+	"github.com/sineto/data-structs/linear_search"
 )
 
 func main() {
+	// LIFO structure
 	stack := lifo.NewStack()
 	stack.Push(&lifo.Item{Value: 1})
 	stack.Push(&lifo.Item{Value: 2})
@@ -19,6 +21,7 @@ func main() {
 		fmt.Println(stack.Items)
 	}
 
+	// FIFO structure
 	queue := fifo.NewQueue()
 	queue.Push(&fifo.Item{Value: 1})
 	queue.Push(&fifo.Item{Value: 2})
@@ -29,4 +32,10 @@ func main() {
 		queue.Shift()
 		fmt.Println(queue.Items)
 	}
+
+	// Linear Search
+	arr := []int{1, 2, 3, 4, 5, 10, 1020, 1234, 0}
+	fmt.Println(linear_search.Search(arr, 10))
+	fmt.Println(linear_search.Search(arr, 1))
+	fmt.Println(linear_search.Search(arr, 0))
 }
